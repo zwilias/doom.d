d5e2be6219e70cdc89f78b94a9a40c2e1c2343ce
ilias <mail@ilias.xyz>
2022-01-24 18:12:16 +0100

    Enable hcl-mode for terraform files

--- config.el
+++ config.el
@@ -101,3 +101,4 @@ current frame."
   (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
   (global-set-key (kbd "<mouse-5>") 'scroll-up-line))
 
+(add-to-list 'auto-mode-alist '("\\.tf\\'" . hcl-mode))
